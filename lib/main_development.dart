b27b44130a3c4d1721961fb0dff90db7081f42bf
HadiZD01 <zidanihadi259@gmail.com>
2026-02-06 12:51:00 +0100

    -added launch json for flavors config -added dev and prod flavors for Andorid -changed android app name for each flavor -added two mainf for each flavor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,8 +2,10 @@ import 'package:appclean/core/di/dependency_injection.dart';
 import 'package:appclean/core/routing/app_router.dart';
 import 'package:appclean/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   setupDependencyInjection();
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(appRouter: AppRouter()));
 }
