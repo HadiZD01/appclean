bfd6b99a8b198ee0c270cc40fb613edc42675f70
HadiZD01 <zidanihadi259@gmail.com>
2026-02-20 15:56:39 +0100

    -shimmer loading advanced for image

--- lib/feature/home/ui/widgets/doctors_list_view_item.dart
+++ lib/feature/home/ui/widgets/doctors_list_view_item.dart
@@ -1,8 +1,12 @@
 import 'package:appclean/core/helpers/spacing.dart';
+import 'package:appclean/core/theming/colors.dart';
 import 'package:appclean/core/theming/styles.dart';
 import 'package:appclean/feature/home/data/specialization_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
 
 class DoctorsListViewItem extends StatelessWidget {
   final Doctors? doctorsModel;
@@ -15,15 +19,43 @@ class DoctorsListViewItem extends StatelessWidget {
       decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
       child: Row(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadiusGeometry.circular(12),
-            child: Image.asset(
-              'assets/images/doctor_pattern.png',
+          // ClipRRect(
+          //   borderRadius: BorderRadiusGeometry.circular(12),
+          //   child: Image.asset(
+          //     'assets/images/doctor_pattern.png',
+          //     width: 110.w,
+          //     height: 120.h,
+          //     cacheWidth: 250,
+          //     alignment: AlignmentGeometry.topCenter,
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
+          CachedNetworkImage(
+            imageUrl:
+                "assets/images/doctor_pattern.png",
+            progressIndicatorBuilder: (context, url, downloadProgress) {
+              return Shimmer.fromColors(
+                baseColor: ColorsApp.lighgray,
+                highlightColor: Colors.white,
+                child: Container(
+                  width: 110.w,
+                  height: 120.h,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            imageBuilder: (context, imageProvider) => Container(
               width: 110.w,
               height: 120.h,
-              cacheWidth: 250,
-              alignment: AlignmentGeometry.topCenter,
-              fit: BoxFit.cover,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12.0),
+                image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+              ),
             ),
           ),
           horizontalSpace(14),
@@ -32,15 +64,18 @@ class DoctorsListViewItem extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  doctorsModel!.name ??'Name',
+                  doctorsModel!.name ?? 'Name',
                   style: TextStyles.font18DarkBlueBold,
                   overflow: TextOverflow.ellipsis,
                 ),
                 verticalSpace(5),
-                Text('${doctorsModel?.degree}||${doctorsModel?.phone}', style: TextStyles.font12GrayMedium),
+                Text(
+                  '${doctorsModel?.degree}||${doctorsModel?.phone}',
+                  style: TextStyles.font12GrayMedium,
+                ),
                 verticalSpace(5),
                 Text(
-                  doctorsModel?.email??'Email',
+                  doctorsModel?.email ?? 'Email',
                   style: TextStyles.font12GrayMedium,
                 ),
               ],
